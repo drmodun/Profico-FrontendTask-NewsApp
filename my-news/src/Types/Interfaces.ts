dcee4d3b67cbd7485e070d5bda1cc988b5674157
drmodun <jan.modun.st@gmail.com>
2023-07-07 02:55:43 +0200

    made images a lot better

--- my-news/src/Types/Interfaces.ts
+++ my-news/src/Types/Interfaces.ts
@@ -78,7 +78,7 @@ export interface ArticleView{
 
 export const ArticleToArticleView = (article: Article): ArticleView => {
     console.log(article.multimedia);
-    const thumbail = article.multimedia.filter((media: Mutimedia) => {console.log(media.subtype);return media.subtype === 'thumbnail'});
+    const thumbail = article.multimedia.filter((media: Mutimedia) => {console.log(media.subtype);return media.subtype === 'jumbo'});
     const thumbnailURL: string = thumbail.length ? "https://static01.nyt.com/" + thumbail[0].url : "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/1024px-No_image_available.svg.png";
     const articleView: ArticleView = {
         web_url: article.web_url,
