3a395944c9079094e4e130383381f2189621150f
drmodun <jan.modun.st@gmail.com>
2023-07-08 20:07:07 +0200

    added search functionality

--- my-news/src/App.tsx
+++ my-news/src/App.tsx
@@ -14,12 +14,19 @@ import { GetFavourites, favourites } from "./Types/LocalFunctions";
 import { Categories } from "./Types/CategoryAssetList";
 import Category from "./Components/Category";
 import LatestNews from "./Components/LatestNews";
+import MyNews from "./Assets/svg/MyNews.svg";
+import Search from "./Assets/svg/Search.svg";
 
 const EmptyArticle: ArticleView[] = [];
 const EmptyNews: News[] = [];
 const App: React.FC = () => {
   const [articles, setArticles] = React.useState(EmptyArticle);
   const [news, setNews] = React.useState(EmptyNews);
+  const [search, setSearch]: [
+    string,
+    React.Dispatch<React.SetStateAction<string>>
+  ] = React.useState("");
+
   const [errorMessages, setErrorMessages]: [
     string,
     React.Dispatch<React.SetStateAction<string>>
@@ -80,51 +87,70 @@ const App: React.FC = () => {
 
   return (
     <div className="App">
-      <div className="header">
-        <span className="title">My News</span>
-      </div>
-      <div className=""></div>
-      <div className="body">
-        <div className="category-selector">
-          <span className="title">Categories</span>
-          <div className="categories">
-            {Categories.map((categoryToMap: CategoryInfo) => (
-              <Category
-                Name={categoryToMap.Name}
-                ImageOn={categoryToMap.ImageOn}
-                ImageOff={categoryToMap.ImageOff}
-                setCategory={setCategory}
-                isActive={categoryToMap.Name === category}
-              />
-            ))}
+      <div className="container">
+        <div className="header">
+          <img src={MyNews} alt="MyNews" />
+          <div className="search">
+            <img src={Search} alt="Search" />
+            <input
+              type="text"
+              placeholder="Search"
+              onChange={(e) => setSearch(e.target.value)}
+              value={search}
+            />
+            <button>
+              <p>Search</p>
+            </button>
           </div>
         </div>
-        <div className="content">
-          {articles
-            .filter((article) => {
-              if (category === "Favourites") {
-                return isFavourite(article.id);
-              }
-              return article.category === category || category === "Home";
-            })
-            .sort((a, b) => {
-              return a.pub_date > b.pub_date ? -1 : 1;
-            })
-            .map((article: ArticleView) => (
-              <ArticleComponent
-                Article={article}
-                toggleBookmark={toggleBookmark}
-                isFavourite={isFavourite(article.id)}
-              />
-            ))}
-        </div>
-        <LatestNews NewsList={news} />
-        {errorMessages !== "" && (
-          <div className="error">
-            {errorMessages +
-              "if this is your first time opening the app, you might need to get temporary authorization from the proxy server, just visit the page "}
+        <div className="body">
+          <div className="category-selector">
+            <span className="title">Categories</span>
+            <div className="categories">
+              {Categories.map((categoryToMap: CategoryInfo) => (
+                <Category
+                  Name={categoryToMap.Name}
+                  ImageOn={categoryToMap.ImageOn}
+                  ImageOff={categoryToMap.ImageOff}
+                  setCategory={setCategory}
+                  isActive={categoryToMap.Name === category}
+                />
+              ))}
+            </div>
           </div>
-        )}
+          <div className="content">
+            {articles
+              .filter((article) => {
+                if (category === "Favourites") {
+                  return isFavourite(article.id);
+                }
+                return (
+                  (article.category === category || category === "Home") &&
+                  (article.headline
+                    .toLowerCase()
+                    .includes(search.toLowerCase()) ||
+                    search === "")
+                );
+              })
+              .sort((a, b) => {
+                return a.pub_date > b.pub_date ? -1 : 1;
+              })
+              .map((article: ArticleView) => (
+                <ArticleComponent
+                  Article={article}
+                  toggleBookmark={toggleBookmark}
+                  isFavourite={isFavourite(article.id)}
+                />
+              ))}
+          </div>
+          <LatestNews NewsList={news} />
+          {errorMessages !== "" && (
+            <div className="error">
+              {errorMessages +
+                "if this is your first time opening the app, you might need to get temporary authorization from the proxy server, just visit the page https://cors-anywhere.herokuapp.com/corsdemo"}
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
